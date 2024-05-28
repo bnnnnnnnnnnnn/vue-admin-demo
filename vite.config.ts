630fb1fa87cc08646d0b610b496f52056d24e24a
bnnnnnnnn <601108790@qq.com>
2024-05-28 19:00:51 +0800

    feat:mock安装与配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,35 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueDevTools from "vite-plugin-vue-devtools";
+import { viteMockServe } from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), VueDevTools()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  // scss 全局变量配置
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/assets/styles/variable.scss";',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      VueDevTools(),
+      viteMockServe({
+        // default
+        mockPath: "mock",
+        enable: command === "serve",
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-  },
+
+    // scss 全局变量配置
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/assets/styles/variable.scss";',
+        },
+      },
+    },
+  };
 });
