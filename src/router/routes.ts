2878f46c1924d0b86cdd74479f824246598f71e4
bnnnnnnnn <601108790@qq.com>
2024-05-29 18:20:18 +0800

    feat：常量路由配置+api接口统一管理

---
+++ src/router/routes.ts
@@ -0,0 +1,24 @@
+// 对外暴露配置路由 常量路由
+import HomeView from "../views/home/index.vue";
+export const constanTRouter = [
+  {
+    path: "/",
+    name: "home", // 命名路由
+    component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "login", // 命名路由
+    component: () => import("../views/login/index.vue"),
+  },
+  {
+    path: "/404",
+    name: "404", // 命名路由
+    component: () => import("../views/404/index.vue"),
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/404",
+    name: "any",
+  },
+];
