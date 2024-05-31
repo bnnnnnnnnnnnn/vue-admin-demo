a93c2f0cf5601c5cdabe2ca4efdaccf6e2b5b3b6
bnnnnnnnn <601108790@qq.com>
2024-05-31 13:53:37 +0800

    feat:登录静态页面已完成

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
-
-
 <template>
   <div>
-    <div class="test">app根组件</div>
     <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import { reqLogin } from './api/user/index';
-
-reqLogin({username:'admin',password:'111111'}).then((res)=>{
-  console.log(res,'res');
-  
-})
+import { RouterView } from "vue-router";
+import { reqLogin } from "./api/user/index";
 </script>
-<style scoped lang='scss'>
-.test{
+<style scoped lang="scss">
+.test {
   color: $color;
 }
 </style>
