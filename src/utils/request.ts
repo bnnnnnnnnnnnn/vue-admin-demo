db449164bb7615cbaacd31f965a8acc01715a6e3
bnnnnnnnn <601108790@qq.com>
2024-06-02 11:24:50 +0800

    fix：登录bug修复

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,11 @@
 // 进行axios 二次封装 ：使用请求与响应拦截
 import axios from "axios";
-import { ElNotification, ElMessageBox, ElMessage, ElLoading } from 'element-plus'
+import {
+  ElNotification,
+  ElMessageBox,
+  ElMessage,
+  ElLoading,
+} from "element-plus";
 // 第一步：利用axios对象的create方法，去创建axios实例（配置：基础路径，超时的时间
 let request = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_API,
@@ -9,8 +14,8 @@ let request = axios.create({
 // 第二步 ：添加请求与响应拦截器
 request.interceptors.request.use((config) => {
   // config配置对象，headers属性请求头
-  console.log(config);
-  config.headers.token='123'
+  // console.log(config);
+  config.headers.token = "123";
   //返回配置对象
   return config;
 });
@@ -18,8 +23,7 @@ request.interceptors.request.use((config) => {
 request.interceptors.response.use(
   (response) => {
     // 成功的回调
-    
-    
+
     //返回配置对象
     return response.data;
   },
@@ -27,18 +31,18 @@ request.interceptors.response.use(
     // 失败的回调：处理http网络错误
     // 定义一个变量：存储网络错误信息
     let status = error.response.status;
-    console.log('err' + error)
-    let { message } = error
+    console.log("err" + error);
+    let { message } = error;
     if (message == "Network Error") {
-      message = "后端接口连接异常"
+      message = "后端接口连接异常";
     } else if (message.includes("timeout")) {
-      message = "系统接口请求超时"
+      message = "系统接口请求超时";
     } else if (message.includes("Request failed with status code")) {
-      message = "系统接口" + message.substr(message.length - 3) + "异常"
+      message = "系统接口" + message.substr(message.length - 3) + "异常";
     }
-    ElMessage({ message: message, type: 'error', duration: 5 * 1000 })
-    return Promise.reject(error)
+    ElMessage({ message: message, type: "error", duration: 5 * 1000 });
+    return Promise.reject(error);
   }
 );
 // 最后 对外暴漏
-export default request
+export default request;
