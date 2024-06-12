cf1aa8f9a61cedd235d48023125221ffda8bf39e
bnnnnnnnn <601108790@qq.com>
2024-06-12 18:53:15 +0800

    feat：左侧菜单静态页面已完成

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="layout_container">
     <!-- 左侧菜单 -->
-    <div class="layout_slider"></div>
+    <div class="layout_slider">
+      <div class="layout_slider_logo">运营平台</div>
+      <el-scrollbar class="scrollbar">
+        <!-- 菜单 -->
+        <Menu />
+      </el-scrollbar>
+    </div>
     <!-- 顶部导航 -->
     <div class="layout_tab"></div>
     <!-- 内容展示区 -->
@@ -9,7 +15,9 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts" name="layout">
+import Menu from "./meau/index.vue";
+</script>
 
 <style scoped lang="scss">
 .layout_container {
@@ -19,6 +27,18 @@
     width: $base-menu-width;
     height: 100vh;
     background: $base-menu-background;
+
+    .layout_slider_logo {
+      color: #ffff;
+      font-size: 26px;
+      display: flex;
+      justify-content: center;
+      padding: 10px 0;
+    }
+    .scrollbar {
+      height: calc(100vh - 55px);
+      padding: 0 5px;
+    }
   }
   .layout_tab {
     position: fixed;
@@ -26,7 +46,7 @@
     height: $base-tab-hight;
     top: 0;
     left: $base-menu-width;
-    background-color: bisque;
+    // background-color: bisque;
   }
   .layout_main {
     position: fixed;
@@ -34,7 +54,8 @@
     height: calc(100% - $base-tab-hight);
     top: $base-tab-hight;
     left: $base-menu-width;
-    background-color: rgb(99, 59, 192);
+    padding: 20px;
+    overflow: auto;
   }
 }
 </style>
