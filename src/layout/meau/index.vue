7af6ed03a24e20a2d3f6a097a2b63aa7fc083237
bnnnnnnnn <601108790@qq.com>
2024-06-13 18:39:03 +0800

    feat：菜单根据返回的动态路由生成

--- src/layout/meau/index.vue
+++ src/layout/meau/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-menu
-      active-text-color="#ffd04b"
+      active-text-color="#409eff"
       background-color="#545c64"
       class="el-menu-vertical-demo"
       default-active="2"
@@ -9,10 +9,15 @@
       @open="handleOpen"
       @close="handleClose"
     >
+      <!-- 根据路由动态生成菜单 -->
+      <!-- <el-menu-item index="0">
+        <el-icon><location /></el-icon>
+        <span>首页</span></el-menu-item
+      >
       <el-sub-menu index="1">
         <template #title>
-          <el-icon><location /></el-icon>
-          <span>Navigator One</span>
+          <el-icon><setting /></el-icon>
+          <span>设置</span>
         </template>
         <el-menu-item-group title="Group One">
           <el-menu-item index="1-1">item one</el-menu-item>
@@ -25,14 +30,23 @@
           <template #title>item four</template>
           <el-menu-item index="1-4-1">item one</el-menu-item>
         </el-sub-menu>
-      </el-sub-menu>
+      </el-sub-menu> -->
     </el-menu>
   </div>
 </template>
 
 <script setup lang="ts" name="menu">
+// 获取用户相关的小仓库
+import  useUserStore  from "@/stores/modules/user";
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from "@element-plus/icons-vue";
+let userStore = useUserStore();
 const handleOpen = () => {};
 const handleClose = () => {};
 </script>
-
+ 
 <style lang="scss" scoped></style>
--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -5,11 +5,13 @@ import type { loginForm, loginResponseData } from "@/api/user/type";
 import type { UserState } from "./types/user";
 //引入本地储存的工具方法
 import { SET_TOKEN, GET_TOKEN } from "@/utils/token";
+import {constanTRouter} from '@/router/routes' // 仓库储存生成菜单需要的数组
 // 创建用户小仓库
 const useUserStore = defineStore("User", {
   state: (): UserState => {
     return {
       token: GET_TOKEN(), // 用户的唯一标识
+      menuRoutes:constanTRouter,// 常量路由
     };
   },
   // 异步、逻辑的地方
