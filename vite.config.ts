a7bd192c7b5cf87485ff54168836b2ced2f07a03
Bnnnnnnnnnnnnn <601108790@qq.com>
2025-03-31 17:42:45 +0800

    chore:使用tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from '@tailwindcss/vite'
-// import { viteMockServe } from "vite-plugin-mock";  
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -12,26 +11,11 @@ export default defineConfig(({ command }) => {
       vue(),
       VueDevTools(),
       tailwindcss(),
-      // viteMockServe({
-      //   // default
-      //   mockPath: "mock",
-      //   enable: command === "serve",
-      // }),
     ],
     envDir: './', // 确保 Vite 读取根目录的 .env 文件
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-
-    // scss 全局变量配置
-    css: {
-      preprocessorOptions: {
-        scss: {
-          javascriptEnabled: true,
-          additionalData: '@import "./src/assets/styles/variable.scss";',
-        },
       },
     },
   };
